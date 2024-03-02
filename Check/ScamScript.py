82b2712507a161be287f2501343723b1aa25d014
dbal0503 <108775943+dbal0503@users.noreply.github.com>
2024-03-02 17:23:07 -0500

    Added finetuned distilbert model and pertaining datasets, with script to make predictions

---
+++ Check/ScamScript.py
@@ -0,0 +1,21 @@
+import os
+
+import torch
+
+from transformers import DistilBertForSequenceClassification, DistilBertTokenizerFast
+path = os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + "\Model"
+
+tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+
+model = DistilBertForSequenceClassification.from_pretrained(path + "\\trained1")
+
+def scamcheck(text):
+    tokenized_message = tokenizer(text, truncation=True, padding=True, return_tensors="pt")
+    with torch.no_grad():
+        outputs = model(**tokenized_message)
+        predicted_label = torch.argmax(outputs.logits, dim=1).item()
+
+    print(predicted_label)
+
+
+scamcheck("INTERAC e-Transfer: JACQUELINE ANDRADE SANCHEZ sent you money. See https://et.interac.ca/sl/wzVjShR98WIy. Data rates may apply")
