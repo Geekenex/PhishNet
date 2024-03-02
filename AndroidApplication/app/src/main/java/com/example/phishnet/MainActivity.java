716576c4c340adad63f2ab09b49c6095cf6fdafe
JosephAmbayec <44503159+JosephAmbayec@users.noreply.github.com>
2024-03-02 10:14:48 -0500

    added permission requesting

--- AndroidApplication/app/src/main/java/com/example/phishnet/MainActivity.java
+++ AndroidApplication/app/src/main/java/com/example/phishnet/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.phishnet;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.provider.Telephony;
 import android.view.View;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -23,7 +28,7 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements SMSReceiver.MessageListenerInterface {
-
+    private static final int RECEIVE_SMS_CODE = 100;
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
@@ -36,6 +41,7 @@ public class MainActivity extends AppCompatActivity implements SMSReceiver.Messa
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
+        checkPermission(Manifest.permission.RECEIVE_SMS, RECEIVE_SMS_CODE);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
@@ -83,5 +89,37 @@ public class MainActivity extends AppCompatActivity implements SMSReceiver.Messa
     public void messageReceived(String message) {
         // Send to server
         Toast.makeText(this.getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
+    // Function to check and request permission.
+    public void checkPermission(String permission, int requestCode)
+    {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_DENIED) {
+
+            // Requesting the permission
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] { permission }, requestCode);
+        }
+        else {
+            Toast.makeText(MainActivity.this, "Permission already granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode,
+                permissions,
+                grantResults);
+
+        if (requestCode == RECEIVE_SMS_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "Receive SMS Permission Granted", Toast.LENGTH_SHORT) .show();
+            }
+            else {
+                Toast.makeText(MainActivity.this, "Receive SMS  Permission Denied", Toast.LENGTH_SHORT) .show();
+            }
+        }
     }
 }
